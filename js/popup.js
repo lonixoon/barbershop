ac30676af9c13156a5db5858cba6397cb62304e4
lonixoon <soinov.alexey@yandex.ru>
2017-03-09 22:23:03 +0700

    добавил popup.js на все страницы

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,12 @@
 var link = document.querySelector(".login");
 var popup_login = document.querySelector(".modal-user-block");
-var close = popup_login.querySelector(".modal-block-close");
+var close_login = popup_login.querySelector(".modal-block-close");
+
+var link_map = document.querySelector(".popup_map");
+var popap_map = document.querySelector(".modal-map");
+var close_map = popap_map.querySelector(".modal-block-close");
+
 var overlay = document.querySelector(".modal-overlay");
-var map = document.querySelector(".modal-map")
 
 // Начало Блок ввода логина и пароля
 link.addEventListener("click", function(event) {
@@ -11,21 +15,23 @@ link.addEventListener("click", function(event) {
   overlay.classList.add("modal-show");
 });
 
-close.addEventListener("click", function(event) {
+close_login.addEventListener("click", function(event) {
   event.preventDefault();
   popup_login.classList.remove("modal-show");
   overlay.classList.remove("modal-show");
 });
 // Конец Блок ввода логина и пароля
 
-link.addEventListener("click", function(event) {
+// Начало Блок карта
+link_map.addEventListener("click", function(event) {
   event.preventDefault();
-  popup_login.classList.add("modal-show");
+  popap_map.classList.add("modal-show");
   overlay.classList.add("modal-show");
 });
 
-close.addEventListener("click", function(event) {
+close_map.addEventListener("click", function(event) {
   event.preventDefault();
-  popup_login.classList.remove("modal-show");
+  popap_map.classList.remove("modal-show");
   overlay.classList.remove("modal-show");
 });
+// Конец Блок карта
